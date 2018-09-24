850843edebc9e39314e65dca08511f7e53b58e64
chakrober <sschakrober@gmail.com>
2018-09-24 20:59:24 -0300

    commit5

---
+++ Uso del IF/Uso del IF/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uso_del_IF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            string valor = "";
+
+            Console.WriteLine("Uso del IF");
+
+            Console.Write("Ingrese un numero: ");
+            valor = Console.ReadLine();
+            numero = Convert.ToInt32(valor);
+
+            if (numero >= 0)
+            {
+                Console.WriteLine("El numero {0} es Positivo", numero);
+            }
+            if (numero <= 0)
+            {
+                Console.WriteLine("El numero {0} es Negativo", numero);
+            }
+        }
+    }
+}
