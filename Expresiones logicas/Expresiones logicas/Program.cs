3b29c1be19e63086c05856df30147ec5c76993ff
chakrober <sschakrober@gmail.com>
2018-09-25 19:10:24 -0300

    commit9

---
+++ Expresiones logicas/Expresiones logicas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expresiones_logicas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            bool permiso = false;
+            string valor = "";
+
+            Console.WriteLine("Programa que al ingresar la edad nos diga si puede conducir o no");
+
+            Console.Write("Ingrese su edad: ");
+            valor = Console.ReadLine();
+            edad = Convert.ToInt32(valor);
+
+            Console.Write("Tienes el permiso de tus padres? (true/false): ");
+            valor = Console.ReadLine();
+            permiso = Convert.ToBoolean(valor);
+
+            if ( edad > 18 || (edad >= 16 & permiso == true))
+            {
+                Console.WriteLine("puedes conducir");
+            }
+            else
+            {
+                Console.WriteLine("No puedes conducir");
+            }
+        }
+    }
+}
---
+++ SWITCH/SWITCH/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWITCH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a = 0.0f;
+            float b = 0.0f;
+            float resultado = 0.0f;
+            string valor = "";
+            int opcion = 0;
+
+            Console.WriteLine("Programa para elegir una opcion con Switch: Suma, Resta, Division, Multiplicacion");
+
+            Console.WriteLine("1. Suma");
+            Console.WriteLine("2. Resta");
+            Console.WriteLine("3. Division");
+            Console.WriteLine("4. Multiplicacion");
+
+            Console.Write("Que operacion desea realizar: ");
+            valor = Console.ReadLine();
+            opcion = Convert.ToInt32(valor);
+
+            Console.Write("Ingrese el primer numero: ");
+            valor = Console.ReadLine();
+            a = Convert.ToSingle(valor);
+
+            Console.Write("Ingrese el segundo numero: ");
+            valor = Console.ReadLine();
+            b = Convert.ToSingle(valor);
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado = a + b;
+                    break;
+                case 2:
+                    resultado = a - b;
+                    break;
+                case 3:
+                    if (b != 0)
+                        resultado = a / b;
+                    else
+                        Console.WriteLine("Divisor no valido");
+                    break;
+                case 4:
+                    resultado = a * b;
+                    break;
+                default:
+                    Console.WriteLine("La Opcion no es valida");
+                    break;
+            }
+
+            Console.WriteLine("El resultado es: {0}", resultado);
+        }
+    }
+}
