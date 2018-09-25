079abbb3f945c1ba6656863ef9a908b0c70ece59
chakrober <sschakrober@gmail.com>
2018-09-24 21:09:50 -0300

    commit6

---
+++ Bloque de codigo con IF Division/Bloque de codigo con IF Division/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bloque_de_codigo_con_IF_Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float dividendo = 0.0f;
+            float divisor = 0.0f;
+            float resultado = 0.0f;
+            string valor = "";
+
+            Console.Write("Ingrese el Dividendo: ");
+            valor = Console.ReadLine();
+            dividendo = Convert.ToSingle(valor);
+
+            Console.Write("Ingrese el Divisor: ");
+            valor = Console.ReadLine();
+            divisor = Convert.ToSingle(valor);
+
+            if (divisor != 0.0f)
+            {
+                resultado = dividendo / divisor;
+                Console.WriteLine("El resultado de la division es: {0}", resultado);
+            }
+        }
+    }
+}
