846dc6970557e55559464d9e9bcdf7cfed8bb3cd
chakrober <sschakrober@gmail.com>
2018-09-26 19:51:11 -0300

    commit 10

---
+++ Ejercicio 3/Ejercicio 3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            string valor = "";
+
+            Console.WriteLine("Hacer un programa que le pida al usuario un numero y la computadora diga si es par o impar");
+
+            Console.Write("Ingrese un numero y te dire si es par o impar: ");
+            valor = Console.ReadLine();
+            numero = Convert.ToInt32(valor);
+
+            if (numero %2 == 0)
+            {
+                Console.WriteLine("El numero {0} es Par", numero);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} es Impar", numero);
+            }
+        }
+    }
+}
---
+++ Ejercicio/Ejercicio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string valor = "";
+            int dia = 0;
+
+            Console.WriteLine("Hacer un programa que le pida un numero del 1 al 7 y escribba el nombre del dia que corresponda ese numero en la semana.");
+            valor = Console.ReadLine();
+            dia = Convert.ToInt32(valor);
+
+            switch (dia)
+            {
+                case 1:
+                    Console.Write("Lunes");
+                    break;
+                case 2:
+                    Console.Write("Martes");
+                    break;
+                case 3:
+                    Console.Write("Miercoles");
+                    break;
+                case 4:
+                    Console.Write("Jueves");
+                    break;
+                case 5:
+                    Console.Write("Viernes");
+                    break;
+                case 6:
+                    Console.Write("Sabado");
+                    break;
+                case 7:
+                    Console.Write("Domingo");
+                    break;
+                default:
+                    Console.Write("La opcion no es Valida");
+                    break;
+            }
+            
+        }
+    }
+}
