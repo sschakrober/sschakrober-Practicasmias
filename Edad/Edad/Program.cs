d20d9df77c21d36aa2891b1b02954d9fc77d3cc4
chakrober <sschakrober@gmail.com>
2018-09-26 22:17:52 -0300

    commit 11

---
+++ Edad/Edad/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Edad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 0;
+            string valor = "";
+
+            Console.WriteLine("Hacer un programa para saber si sos mayor o menor de Edad");
+
+            Console.Write("Ingrese su Edad: ");
+            valor = Console.ReadLine();
+            edad = Convert.ToInt32(valor);
+
+            if ( edad >= 18)
+            {
+                Console.WriteLine("Eres Mayor de Edad");
+            }
+            else
+            {
+                Console.WriteLine("Eres Menor de Edad");
+            }
+        }
+    }
+}
