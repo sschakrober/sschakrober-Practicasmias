9d93e593c306055c17d0a7ea324d769504f879e6
Chakrober <42191972+sschakrober@users.noreply.github.com>
2018-11-01 17:17:50 -0300

    commit12

---
+++ Empleados/Empleados/Models/Empleado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Empleados.Models
+{
+    public class Empleado
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public int Antiguedad { get; set; }
+        public int Edad { get; set; }
+    }
+
+    public class EmpleadoDBContext : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+    }
+}
---
+++ Empleados/Empleados/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Empleados.Startup))]
+namespace Empleados
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
